54a70160f511e43195e2399df348039ccca3f460
walle <walllle.ru@gmail.com>
2025-06-29 22:33:14 +0500

    add login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,18 @@
-import { Card } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
 
 export default function Login() {
   return (
     <div className='flex justify-center items-center h-[100vh]'>
-        <Card className='w-[20em]'>
-            <Input type="email" placeholder='Email'/>
-            <Input type='password' placeholder='Password'/>
+        <Card className='w-[30em] gap-1 p-5'>
+          <CardTitle className='text-center'>Login</CardTitle>
+            <Label>Email</Label>
+            <Input type="email"/>
+            <Label>Password</Label>
+            <Input type='password'/>
+            <Button type='submit'>Login</Button>
         </Card>
     </div>
   )
