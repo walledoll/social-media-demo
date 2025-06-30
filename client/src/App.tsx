1211ceb9307f7b48ebc3d3895682bac2bc17e2c2
walle <walllle.ru@gmail.com>
2025-06-30 12:05:48 +0500

    add register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='one' element={<Login />}/>
+          <Route path='login' element={<Login />} />
         </Routes>
       </BrowserRouter>
 
