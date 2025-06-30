e26993b0b46dad77a3ce63a786e32fc15da9bff8
walle <walllle.ru@gmail.com>
2025-06-30 11:59:49 +0500

    add first route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import Login from "./pages/Login"
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        
+        <Routes>
+          <Route path='one' element={<Login />}/>
+        </Routes>
       </BrowserRouter>
-      <Login></Login>
+
     </>
   )
 }
