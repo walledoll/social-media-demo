b9ac5c41a958fbe99d9e0b3b7ff65aac1a8dc60c
walle <walllle.ru@gmail.com>
2025-07-03 20:01:01 +0500

    add chat module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CahtModule } from './caht/caht.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [CahtModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
